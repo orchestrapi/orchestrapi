67e16eb7f1411c8321ead8ba72f84e3bc73877c1
Francisco Manzano Magaña <keoh77@gmail.com>
2018-10-16 23:38:43 +0200

    wsgi conf

--- core/wsgi.py
+++ core/wsgi.py
@@ -3,7 +3,7 @@ import django
 from django.core.handlers.wsgi import WSGIHandler
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.prod")
 django.setup(set_prefix=False)
 
 application = WSGIHandler()
