22e716ec0a975fbad1fffd7dbcd60380ddc69651
Francisco Manzano Magaña <keoh77@gmail.com>
2018-10-16 21:07:44 +0200

    wsgi conf

--- orchestrapi/core/settings/prod.py
+++ orchestrapi/core/settings/prod.py
@@ -1,5 +1,10 @@
 from .base import *
 
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    'orchestra.paquito.ninja'
+]
 
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, '../media')
 STATIC_ROOT = os.path.join(PROJECT_ROOT, '../static')
