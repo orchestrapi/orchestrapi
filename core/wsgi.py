85b12ab400bc14e02d8ddda415aba2ad25d0a9da
Francisco Manzano Magaña <keoh77@gmail.com>
2018-10-16 21:51:38 +0200

    wsgi file

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for core project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
-"""
-
 import os
+import django
+from django.core.handlers.wsgi import WSGIHandler
 
-from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.prod')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.production")
+django.setup(set_prefix=False)
 
-application = get_wsgi_application()
+application = WSGIHandler()
