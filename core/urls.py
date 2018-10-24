7183f0dc1164fcf626603477757b7294671ee099
Francisco Manzano <fmanzano@qdqmedia.com>
2018-10-24 12:19:09 +0200

    Index view

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from django.views.generic.base import TemplateView
+
 urlpatterns = [
+    path('', TemplateView.as_view(template_name="index.html")),
     path('admin/', admin.site.urls),
 ]
