cd9ff32506d57c260ec18c96537d5bc69235b7ce
Francisco Manzano Magaña <keoh77@gmail.com>
2018-10-31 00:24:58 +0100

    csrf excempt

--- webhooks/views.py
+++ webhooks/views.py
@@ -1,5 +1,8 @@
 
 from django.http import JsonResponse
 
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
 def manage_webhook(request, project_id):
     return JsonResponse({}, status=200)
