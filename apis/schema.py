5764e70dc84bc615ad451ab763ad46a853dec954
Francisco Manzano Magaña <keoh77@gmail.com>
2018-11-30 21:00:05 +0100

    Image schema for GraphQL

--- apis/schema.py
+++ apis/schema.py
@@ -2,10 +2,12 @@ import graphene
 
 from apps.schema import AppQuery
 from containers.schema import ContainerQuery
+from images.schema import ImageQuery
 
 
 class Query(
         AppQuery, ContainerQuery,
+        ImageQuery,
         graphene.ObjectType):
     pass
 
