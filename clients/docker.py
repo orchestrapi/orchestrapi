57570923c4a230d4aa39415c385d63c2d3b3872b
Francisco Manzano Magaña <keoh77@gmail.com>
2019-01-31 21:20:29 +0100

    Create and removes docker networks

--- clients/docker.py
+++ clients/docker.py
@@ -189,3 +189,46 @@ class DockerClient:
             f'{image.image_tag}',
             f'{settings.GIT_PROJECTS_ROUTE}/{git_name}/.']
         return self.call(template)
+
+    def create_network(self, name):
+        return self.client.networks.create(name, driver="bridge")
+
+    def remove_network(self, network):
+        try:
+            network.remove()
+        except docker.errors.APIError:
+            print(f"Error al eliminar la red {network.id}")
+
+    def get_network_by_id(self, network_id, verbose=False):
+        try:
+            return self.client.networks.get(network_id, verbose=verbose)
+        except docker.errors.NotFound:
+            return None
+        except docker.errors.APIError:
+            print(f"Error obteniendo red {network_id}")
+
+    def connect_container_to_network(self, network, container):
+        try:
+            network.connect(container)
+        except docker.errors.APIError:
+            print(
+                f"Error conectando contenedor {container} a la red {network.name}")
+
+    def disconnect_container_to_network(self, network, container, force=False):
+        try:
+            network.disconnect(container, force=force)
+        except docker.errors.APIError:
+            print(
+                f"Error desconectando contenedor {container} a la red {network.name}")
+
+    def list_networks(self):
+        try:
+            return self.client.networks.list(driver="bridge")
+        except docker.errors.APIError:
+            print("Error al listar redes")
+
+    def networks_prune(self, filters=None):
+        try:
+            return self.client.networks.prune(filters=filters)
+        except docker.errors.APIError:
+            print("Error borrando redes sin uso")
--- networks/models.py
+++ networks/models.py
@@ -3,14 +3,26 @@ from django.db import models
 from core.behaviours import SlugableBehaviour, TimestampableBehaviour, UUIDIndexBehaviour
 from clients.docker import DockerClient
 
+from django.db.models.signals import post_delete
 
 class NetworkBase(UUIDIndexBehaviour, SlugableBehaviour, TimestampableBehaviour, models.Model):
 
     name = models.CharField(max_length=30)
+    network_id = models.CharField(max_length=100, blank=True, null=True)
 
     def __init__(self, *args, **kwargs):
         self.dclient = DockerClient()
         super(NetworkBase, self).__init__(*args, *kwargs)
+
+    def get_docker_network(self):
+        if self.network_id:
+            return self.dclient.get_network_by_id(self.network_id)
+
+    def remove_docker_network(self):
+        if self.network_id:
+            network = self.get_docker_network()
+            if network:
+                self.dclient.remove_network(self.get_docker_network())
 
     class Meta:
         abstract = True
@@ -18,5 +30,21 @@ class NetworkBase(UUIDIndexBehaviour, SlugableBehaviour, TimestampableBehaviour,
 
 class NetworkBridge(NetworkBase):
 
+    def save(self, *args, **kwargs):
+        super(NetworkBridge, self).save(*args, **kwargs)
+        if not self.network_id:
+            self.create_network()
+
     def create_network(self):
-        pass
+        network = self.dclient.create_network(self.slug)
+        self.network_id = network.id
+        self.save()
+
+
+# SIGNALS
+
+def delete_docker_network(sender, **kwargs):
+    instance = kwargs.get('instance')
+    instance.remove_docker_network()
+
+post_delete.connect(delete_docker_network, sender=NetworkBridge)
